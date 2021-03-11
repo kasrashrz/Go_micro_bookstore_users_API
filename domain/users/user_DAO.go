8969a6c18b36a4fd60c467960edde99ab023264a
KasTheDeveloper <kasra.shirazi.1234@gmail.com>
2021-03-12 01:48:26 +0330

    practicing on methods, new not found error added for 404 reqs, creating memory on system instead of SQL DB

--- domain/users/user_DAO.go
+++ domain/users/user_DAO.go
@@ -1,1 +1,28 @@
 package users
+
+import (
+	"fmt"
+	"github.com/kasrashrz/Golang_microservice/utils/errors"
+)
+
+var(
+	usersDB = make(map[int64]*User)
+)
+
+func (user User) Get() *errors.RestErr{
+	result := usersDB[user.Id]
+	if result == nil{
+		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
+	}
+
+	user.Id = result.Id
+	user.Email = result.Email
+	user.DateCreated = result.DateCreated
+	user.Firstname = result.Firstname
+	user.Lastname = result.Lastname
+	return nil
+}
+
+func (user User) Create() *errors.RestErr {
+	return nil
+}
